3b18da7fc387eb30a26f51c0ea33d14cf450626d
MJGarrigan21 <MJGarrigan21@gmail.com>
2021-11-10 16:37:51 -0700

    Created database classes for users and posts. Weird error where I can't access class attributes within the database. Will fix tomorrow.

--- flaskapp.py
+++ flaskapp.py
@@ -1,30 +1,60 @@
+from enum import unique
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
+from sqlalchemy.orm.attributes import QueryableAttribute
+from wtforms.validators import Email
 from forms import RegistrationForm, LoginForm
+from datetime import datetime
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'c0d7bf0377e75f40c2431a3e5d9083a2'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', dynamic=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 posts = [
     {
-        'author' : 'Michael Garrigan',
-        'title' : 'First Blog Post',
-        'content' : 'First post content',
-        'date_posted' : 'Nov. 1, 2021'
+        'author': 'Michael Garrigan',
+        'title': 'First Blog Post',
+        'content': 'First post content',
+        'date_posted': 'Nov. 1, 2021'
 
     },
     {
-        'author' : "J.R.R. Tolkien",
-        'title' : "The Flight of the Noldor",
-        'content' : "Yeah we fucked up.",
-        'date_posted' : "YT 1106"
+        'author': "J.R.R. Tolkien",
+        'title': "The Flight of the Noldor",
+        'content': "Yeah we fucked up.",
+        'date_posted': "YT 1106"
 
     }
 ]
+
 
 @app.route("/")
 @app.route("/home")
@@ -36,6 +66,7 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -43,6 +74,7 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -54,6 +86,7 @@ def login():
         else:
             flash(f'Login unsuccsesful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
