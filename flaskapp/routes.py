4362f23f0b39dfda225dd1a5aeede35a138dc5c2
MJGarrigan21 <MJGarrigan21@gmail.com>
2021-12-09 16:38:18 -0700

    added route restriction and redirection, changed plslogin flash message to look better.

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
+from flask_login.utils import login_required
 from flaskapp import app, db, bcrypt
 from flaskapp.forms import RegistrationForm, LoginForm
 from flaskapp.models import User, Post
@@ -58,7 +59,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash(f'Login unsuccsesful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -70,5 +72,6 @@ def logout():
 
 
 @app.route("/account")
+@login_required
 def account():
     return render_template('account.html', title='Account')
